c6ce13e71090fe2584d4e7813fedec3780f21255
半城风雨 <1260211623@qq.com>
2022-02-21 17:19:21 +0800

    更新

--- app/service/ad.go
+++ app/service/ad.go
@@ -7,6 +7,13 @@
 // +----------------------------------------------------------------------
 // | Author: 半城风雨 <easygoadmin@163.com>
 // +----------------------------------------------------------------------
+// | 免责声明:
+// | 本软件框架禁止任何单位和个人用于任何违法、侵害他人合法利益等恶意的行为，禁止用于任何违
+// | 反我国法律法规的一切平台研发，任何单位和个人使用本软件框架用于产品研发而产生的任何意外
+// | 、疏忽、合约毁坏、诽谤、版权或知识产权侵犯及其造成的损失 (包括但不限于直接、间接、附带
+// | 或衍生的损失等)，本团队不承担任何法律责任。本软件框架只能用于公司和个人内部的法律所允
+// | 许的合法合规的软件产品研发，详细声明内容请阅读《框架免责声明》附件；
+// +----------------------------------------------------------------------
 
 /**
  * 广告管理-服务类
@@ -101,7 +108,6 @@ func (s *adService) Add(req *model.AdAddReq, userId int) (int64, error) {
 	entity.EndTime = req.EndTime
 	entity.Status = req.Status
 	entity.Sort = req.Sort
-	entity.Note = req.Note
 	entity.CreateUser = userId
 	entity.CreateTime = gtime.Now()
 	entity.Mark = 1
@@ -159,7 +165,6 @@ func (s *adService) Update(req *model.AdUpdateReq, userId int) (int64, error) {
 	info.EndTime = req.EndTime
 	info.Status = req.Status
 	info.Sort = req.Sort
-	info.Note = req.Note
 	info.UpdateUser = userId
 	info.UpdateTime = gtime.Now()
 
